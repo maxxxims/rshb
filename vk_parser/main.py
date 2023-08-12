209a9065500e7a7acd5afd3fe3003862129fded1
Бойков Максим <boikov.me@phystech.edu>
2023-08-12 08:15:26 +0300

    upd

--- vk_parser/main.py
+++ vk_parser/main.py
@@ -21,7 +21,7 @@ def parse_list_of_groups(parser: Parser, group_list: list):
         parser.set_group(group)
         parser.parse_users_from_group(test=False)
     parser.parse_user_info(n_users='all')
-    parser.save_df('user_m2.csv')
+    parser.save_df('all_users.csv')
         
 
 def parse_all(parser: Parser):
@@ -31,7 +31,7 @@ def parse_all(parser: Parser):
 def find_common_groups(parser: Parser, path_to_file: str):
     parser.load_users(path_to_file)
     parser.parse_common_groups()
-    parser.save_counter(path_to_file='output_data/counter_end.csv')
+    parser.save_counter(path_to_file='output_data/counter.csv')
 
 
 def get_top_common_groups(parser: Parser, save_file: bool = True):
@@ -41,7 +41,7 @@ def get_top_common_groups(parser: Parser, save_file: bool = True):
 
 def get_top_groups_types(parser: Parser):
     #parser.merge_common_groups(groups = ['output_data/counter_2.csv', 'output_data/counter_23.csv'])
-    parser.load_counter('output_data/counter_all.csv')
+    parser.load_counter('output_data/counter.csv')
     parser.get_groups_types()
 
 
@@ -50,27 +50,16 @@ def load_counter(parser: Parser, path_to_file: str):
     cnt = parser.get_counter()
 
 
-def test(parser: Parser):
-    parser.set_users([378256, 678560])
-    parser.parse_user_info(n_users='all')
-    print(parser.df)
-
-
 if __name__ == '__main__':
     token = GET_TOKEN()
     parser = Parser(token)
-    #parse_list_of_groups(parser, group_list=[groups['svoe_rodnoe'], groups['agromp'], groups['esh_derevenskoe']])
-
+    #parse_list_of_groups(parser, [groups['m2-organic'], groups['eco_lakomka'], groups['fermermag'], groups['ecomarket'], groups['apeti']])
     #get_top_groups_types(parser)
-
+    #parser.save_df(path='data_analysis/all_users.csv')
+    
     find_common_groups(parser, path_to_file='data_analysis/all_users_end.csv') #
     get_top_common_groups(parser, save_file=True)
 
 
-    #parse_list_of_groups(parser, [groups['m2-organic'], groups['eco_lakomka'], groups['fermermag'], groups['ecomarket'], groups['apeti']])
-    
-    #get_top_common_groups(parser, save_file = False)
-    #get_top_groups_types(parser)
-
     #load_counter(parser, path_to_file='output_data/counter.csv')
-    #parser.save_df(path='data_analysis/users_all2.csv')
+    
